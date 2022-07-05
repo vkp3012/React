8908651396ba35e13ec79b3afe8d218b1202a53c
vkp3012 <vivekpatel301295@gmail.com>
2022-07-05 21:37:31 +0530

    todo app start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Components/Todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //  <h1>Hello from the other side </h1>
+    // eslint-disable-next-line react/jsx-no-undef
+    <Todo/>
   );
 }
 
 export default App;
+
